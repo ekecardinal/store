5e515d09e13390fd30757d3ef6d5a5faf90dd132
EKE Princewill <ekecardinal@gmail.com>
2019-12-14 09:24:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,3 +38,31 @@ function showSlides1(n, no) {
   }
   x[slideIndex1[no]-1].style.display = "flex";
 }
+
+
+/* Multiple images in a slider */
+/*
+    Carousel
+*/
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+  /*
+      CC 2.0 License Iatek LLC 2018 - Attribution required
+  */
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 5;
+  var totalItems = $('.carousel-item').length;
+
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
